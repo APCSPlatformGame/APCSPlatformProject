22456e3a485ca727bd3e77455f5ae7438d6f5fc5
Screencap <t4canty@users.noreply.github.com>
2020-01-26 12:43:06 -0800

    Update Player.java

--- src/src/Player.java
+++ src/src/Player.java
@@ -7,33 +7,35 @@ import java.awt.Image;
 import java.awt.Rectangle;
 
 public class Player extends Entity implements Paint{
-	private final int halfSize = super.size/2;
-	private final int fullSize = super.size;
+	private final int halfSize = super.size.height/2;
+	private final int fullSize = super.size.height;
 	public int jump = 0;
 	public int accel = 0;
-	private int player_weight = super.size/10;
-	public int health = 3;
+	public int health = 10;
 	public int deaths = 0;
 	private boolean damaged = false;
-	public Player(int size, Image gif) {
+	private boolean bleedingOver = false;
+	private Rectangle futureRectangles[] = new Rectangle[2];
+	public Player(Dimension size, Image gif) {
 		super(size, gif);
-		super.x = 50;
+		super.x = 100;
+		futureRectangles[0] = new Rectangle(x-1, y, 1, size.height);
+		futureRectangles[1] = new Rectangle(x+size.width, y, 1, size.height);
 		// TODO Auto-generated constructor stub
 	}
+	public Rectangle getFront() { return futureRectangles[1];}
+	public Rectangle getBack() { return futureRectangles[0];}
 	
-	public int getPlayerWeight() {
-		return player_weight;
-	}
 	public void moveRight() {
 		if(accel < 20) {
-			accel += 4;
+			accel += 3;
 		}else {
 			accel++;
 		}
 	}
 	public void moveLeft() {
 		if(accel > -20) {
-			accel -= 4;
+			accel -= 3;
 		}else {
 			accel--;
 		}
@@ -42,45 +44,33 @@ public class Player extends Entity implements Paint{
 	public void jump(boolean onGround) {
 		if(onGround) {
 			super.y -= 1;
-			jump = 90;
+			jump = 25;
 		}
 	}
 	
 	public void crouch() {
-		size = halfSize;
-		super.x += halfSize;
+		size.height = halfSize;
 		super.y += halfSize;
 	}
 	public void unCrouch() {
-		size = fullSize;
-		super.x -= halfSize;
+		size.height = fullSize;
 		super.y -= fullSize;
 	}
 	
 	public void paint(Graphics g) {
-		//super.x += super.vx;
-		super.y += super.vy;
-		rect = new Rectangle(super.X(), super.Y(), size, size);
+		y += vy;
+		rect = new Rectangle(x, y, size.width, size.height);
+		futureRectangles[0] = new Rectangle(x-1 + accel, y, 1+accel, size.height);
+		futureRectangles[1] = new Rectangle(x+size.width + accel, y, 1, size.height);
+		g.drawRect(x+size.width + accel, y, 1, size.height);
 		g.setColor(Color.orange);
-		g.fillRect(super.x, super.y, size, size);
+		g.drawImage(gif, super.x, super.y, size.width, size.height, null);
 		g.setColor(Color.blue);
 		g.drawRect(rect.x, rect.y, rect.width, rect.height);
 	}
-	public void gameOver(Graphics g, Dimension screen){
-		if(health==0){
-			g.fillRect(0, 0, screen.width, screen.height);
-			health = 3;
-			super.x = 50;
-			deaths++;
-		}
-		
-	}
-	
-	
-	public void damage(Rectangle ouchMachine){
-		
-		boolean bleedingOver = false;
-		damaged = this.rect.intersects(ouchMachine);
+	public void damage(Rectangle EnemyRect){
+
+		damaged = this.rect.intersects(EnemyRect);
 		if(damaged==true && bleedingOver == false){
 			health--;
 			bleedingOver = true;
@@ -89,6 +79,5 @@ public class Player extends Entity implements Paint{
 			bleedingOver = false;
 		}
 	}
-	
 
 }
