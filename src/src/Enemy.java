6d3fc0dc9bb61cb20c3f96699722a5b5ef58ef1a
Screencap <t4canty@users.noreply.github.com>
2020-01-26 12:34:04 -0800

    Update Enemy.java
    Added paint method

--- src/src/Enemy.java
+++ src/src/Enemy.java
@@ -1,18 +1,30 @@
 package src;
 
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
-
-import com.sun.prism.Image;
+import java.awt.Image;
+import java.awt.Rectangle;
 
 public class Enemy extends Entity implements Paint{
-
-	public Enemy(int size, Image gif, int x, int y) {
+	private int damage;
+	public Enemy(Dimension size, Image gif, int x, int y, int damage) {
 		super(size, gif, x, y);
+		this.damage = damage;
 	}
-
+	
+	
 	@Override
 	public void paint(Graphics g) {
-		// TODO Auto-generated method stub
+		x += vx;
+		this.rect = new Rectangle(x, y, size.width, size.height);
+		faces[0] = new Rectangle(x, y, size.width, 5); // Top Face
+		faces[1] = new Rectangle(x, y-5, 5, size.height-5); // Left Face
+		faces[2] = new Rectangle(x + size.width, y-5, 5, size.height-5); // Right Face
+		faces[3] = new Rectangle(x, y + size.height, size.width, 5); // Bottom Face
+		g.setColor(Color.red);
+		g.fillRect(x, y, size.width, size.height);
+		g.drawImage(gif, x, y, size.width, size.height, null, null );
 		
 	}
 
